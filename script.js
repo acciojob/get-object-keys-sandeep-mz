ca5668e4e84b8eaee3685bd4e74796218a3ec3de
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-11-23 17:41:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+
+// Create an object named 'student' with a 'name' property
+const student = {
+  name: 'John' // Example name
+};
+
+// Add a method 'getKeys()' to the Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Example usage
+console.log(student.getKeys()); // Output: ['name']
